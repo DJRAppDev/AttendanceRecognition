fd702a5c17110a03ebf0d84b98b9be959caf1591
Your NameProjectBM <quderek@yahoo.com>
2018-05-26 19:42:28 -0400

    Add Google Sheets API.

--- app/src/main/java/com/djrappdev/attendancerecognition/MainActivity.java
+++ app/src/main/java/com/djrappdev/attendancerecognition/MainActivity.java
@@ -1,12 +1,24 @@
 package com.djrappdev.attendancerecognition;
 
+import android.Manifest;
+import android.accounts.AccountManager;
+import android.app.Dialog;
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Debug;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -19,6 +31,20 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential;
+import com.google.api.client.googleapis.extensions.android.gms.auth.GooglePlayServicesAvailabilityIOException;
+import com.google.api.client.googleapis.extensions.android.gms.auth.UserRecoverableAuthIOException;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.util.ExponentialBackOff;
+import com.google.api.services.sheets.v4.SheetsScopes;
+import com.google.api.services.sheets.v4.model.Sheet;
+import com.google.api.services.sheets.v4.model.UpdateValuesResponse;
+import com.google.api.services.sheets.v4.model.ValueRange;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -26,11 +52,17 @@ import org.json.JSONObject;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Base64;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
-public class MainActivity extends AppCompatActivity {
+import pub.devrel.easypermissions.AfterPermissionGranted;
+import pub.devrel.easypermissions.EasyPermissions;
+
+public class MainActivity extends AppCompatActivity implements EasyPermissions.PermissionCallbacks {
     private static final int REQUEST_IMAGE_CAPTURE = 1;
     private final String app_id = "858c50a3";
     private final String app_key = "1f30faa8bbb6406231731963e9129191";
@@ -39,6 +71,19 @@ public class MainActivity extends AppCompatActivity {
     private String studentPath;
     private TextView statusText, verifyResult;
     private EditText osisField;
+
+    public String globalOsis = "220080063";
+
+    GoogleAccountCredential mCredential;
+    private Button mCallApiButton;
+
+    static final int REQUEST_ACCOUNT_PICKER = 1000;
+    static final int REQUEST_AUTHORIZATION = 1001;
+    static final int REQUEST_GOOGLE_PLAY_SERVICES = 1002;
+    static final int REQUEST_PERMISSION_GET_ACCOUNTS = 1003;
+
+    private static final String PREF_ACCOUNT_NAME = "accountName";
+    private static final String[] SCOPES = {SheetsScopes.SPREADSHEETS};
 
 
     @Override
@@ -52,6 +97,7 @@ public class MainActivity extends AppCompatActivity {
         takePhoto = findViewById(R.id.takePhoto);
         verifyStudent = findViewById(R.id.verifyStudent);
         verifyResult = findViewById(R.id.verifyResult);
+        mCallApiButton = findViewById(R.id.sheetsAPI);
 
         queue = Volley.newRequestQueue(this);
 
@@ -95,6 +141,59 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+
+        //Adds OnClickListener to connect to Google Sheets API
+        mCallApiButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                verifyResult.setText("");
+                getResultsFromApi();
+                mCallApiButton.setEnabled(false);
+            }
+        });
+
+        verifyResult.setText("Click the Google Sheets API button first.");
+
+        mCredential = GoogleAccountCredential.usingOAuth2(
+                getApplicationContext(), Arrays.asList(SCOPES))
+                .setBackOff(new ExponentialBackOff());
+    }
+
+    private void getResultsFromApi() {
+        if (!isGooglePlayServicesAvailable()) {
+            acquireGooglePlayServices();
+        } else if (mCredential.getSelectedAccountName() == null) {
+            chooseAccount();
+        } else if (!isDeviceOnline()) {
+            verifyResult.setText("No network connection available.");
+        } else {
+            new MakeRequestTask(mCredential).execute();
+        }
+    }
+
+    @AfterPermissionGranted(REQUEST_PERMISSION_GET_ACCOUNTS)
+    private void chooseAccount() {
+        if (EasyPermissions.hasPermissions(
+                this, Manifest.permission.GET_ACCOUNTS)) {
+            String accountName = getPreferences(Context.MODE_PRIVATE)
+                    .getString(PREF_ACCOUNT_NAME, null);
+            if (accountName != null) {
+                mCredential.setSelectedAccountName(accountName);
+                getResultsFromApi();
+            } else {
+                // Start a dialog from which the user can choose an account
+                startActivityForResult(
+                        mCredential.newChooseAccountIntent(),
+                        REQUEST_ACCOUNT_PICKER);
+            }
+        } else {
+            // Request the GET_ACCOUNTS permission via a user dialog
+            EasyPermissions.requestPermissions(
+                    this,
+                    "This app needs to access your Google account (via Contacts).",
+                    REQUEST_PERMISSION_GET_ACCOUNTS,
+                    Manifest.permission.GET_ACCOUNTS);
+        }
     }
 
     //Functions to take photo and save it to internal storage
@@ -120,6 +219,54 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     }
+
+    @Override
+    protected void onActivityResult(
+            int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case REQUEST_GOOGLE_PLAY_SERVICES:
+                if (resultCode != RESULT_OK) {
+                    verifyResult.setText(
+                            "This app requires Google Play Services. Please install " +
+                                    "Google Play Services on your device and relaunch this app.");
+                } else {
+                    getResultsFromApi();
+                }
+                break;
+            case REQUEST_ACCOUNT_PICKER:
+                if (resultCode == RESULT_OK && data != null &&
+                        data.getExtras() != null) {
+                    String accountName =
+                            data.getStringExtra(AccountManager.KEY_ACCOUNT_NAME);
+                    if (accountName != null) {
+                        SharedPreferences settings =
+                                getPreferences(Context.MODE_PRIVATE);
+                        SharedPreferences.Editor editor = settings.edit();
+                        editor.putString(PREF_ACCOUNT_NAME, accountName);
+                        editor.apply();
+                        mCredential.setSelectedAccountName(accountName);
+                        getResultsFromApi();
+                    }
+                }
+                break;
+            case REQUEST_AUTHORIZATION:
+                if (resultCode == RESULT_OK) {
+                    getResultsFromApi();
+                }
+                break;
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        EasyPermissions.onRequestPermissionsResult(
+                requestCode, permissions, grantResults, this);
+    }
+
     private File createImageFile(String OSIS) throws IOException {
         // Create an image file name
         String imageFileName = OSIS;
@@ -133,6 +280,51 @@ public class MainActivity extends AppCompatActivity {
         // Save a file: path for use with ACTION_VIEW intents
         studentPath = image.getAbsolutePath();
         return image;
+    }
+
+    @Override
+    public void onPermissionsGranted(int requestCode, List<String> list) {
+        // Do nothing.
+    }
+
+    @Override
+    public void onPermissionsDenied(int requestCode, List<String> list) {
+        // Do nothing.
+    }
+
+    private boolean isDeviceOnline() {
+        ConnectivityManager connMgr =
+                (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+        return (networkInfo != null && networkInfo.isConnected());
+    }
+
+    private boolean isGooglePlayServicesAvailable() {
+        GoogleApiAvailability apiAvailability =
+                GoogleApiAvailability.getInstance();
+        final int connectionStatusCode =
+                apiAvailability.isGooglePlayServicesAvailable(this);
+        return connectionStatusCode == ConnectionResult.SUCCESS;
+    }
+
+    private void acquireGooglePlayServices() {
+        GoogleApiAvailability apiAvailability =
+                GoogleApiAvailability.getInstance();
+        final int connectionStatusCode =
+                apiAvailability.isGooglePlayServicesAvailable(this);
+        if (apiAvailability.isUserResolvableError(connectionStatusCode)) {
+            showGooglePlayServicesAvailabilityErrorDialog(connectionStatusCode);
+        }
+    }
+
+    void showGooglePlayServicesAvailabilityErrorDialog(
+            final int connectionStatusCode) {
+        GoogleApiAvailability apiAvailability = GoogleApiAvailability.getInstance();
+        Dialog dialog = apiAvailability.getErrorDialog(
+                MainActivity.this,
+                connectionStatusCode,
+                REQUEST_GOOGLE_PLAY_SERVICES);
+        dialog.show();
     }
 
     //Function to convert image into Base64
@@ -198,10 +390,14 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     //TODO: Case checking so app knows what to do with the verification response from Kairos API.
                     public void onResponse(JSONObject response) {
-                        // response
-                        Log.d("Response", response.toString());
+                        // response Add to spreadsheets
+                        Log.d("Response123", response.toString());
+                        //globalOsis = PUT OSIS HERE;
+                        getResultsFromApi();
+
                         try {
                             Log.d("Response", "" + response.getJSONArray("Errors").getJSONObject(0).getInt("ErrCode"));
+                            verifyResult.setText("Internal Error!");
                         } catch (JSONException e) {
                             e.printStackTrace();
                         }
@@ -224,5 +420,127 @@ public class MainActivity extends AppCompatActivity {
             }
         };
         queue.add(postRequest);
+
+    }
+
+    public class MakeRequestTask extends AsyncTask<Void, Void, List<String>> {
+        private com.google.api.services.sheets.v4.Sheets mService = null;
+        private Exception mLastError = null;
+
+        MakeRequestTask(GoogleAccountCredential credential) {
+            HttpTransport transport = AndroidHttp.newCompatibleTransport();
+            JsonFactory jsonFactory = JacksonFactory.getDefaultInstance();
+            mService = new com.google.api.services.sheets.v4.Sheets.Builder(
+                    transport, jsonFactory, credential)
+                    .setApplicationName("Google Sheets API Android Quickstart")
+                    .build();
+        }
+
+        @Override
+        protected List<String> doInBackground(Void... params) {
+            try {
+                return getDataFromApi();
+            } catch (Exception e) {
+                mLastError = e;
+                cancel(true);
+                return null;
+            }
+        }
+
+        private List<String> getDataFromApi() throws IOException {
+            String spreadsheetId = "1kq83ycJEON5R8IuWm-xdtClYGL1thZPA1-qLDf0IVZg";
+            String range = "Sheet1!B:B";
+
+            List<String> results = new ArrayList<String>();
+
+            ValueRange response = this.mService.spreadsheets().values()
+                    .get(spreadsheetId, range)
+                    .execute();
+            List<List<Object>> values = response.getValues();
+
+            if (values != null) {
+                for (List row : values) {
+                    results.add(row.get(0) + "");
+                }
+            }
+
+            int rowCount = 0;
+            for(List row: values){
+                rowCount += 1;
+                if(row.get(0).equals(globalOsis)) {
+                    Log.d("Found123", rowCount + "");
+                    break;
+                }
+            }
+
+            String rangeStatus = "Sheet1!C:C";
+
+            List<String> resultsStatus = new ArrayList<String>();
+
+            ValueRange responseStatus = this.mService.spreadsheets().values()
+                    .get(spreadsheetId, rangeStatus)
+                    .execute();
+            List<List<Object>> valuesStatus = responseStatus.getValues();
+
+            if (valuesStatus != null) {
+                for (List row : valuesStatus) {
+                    resultsStatus.add(row.get(0) + "");
+                }
+            }
+
+            int iteration = 0;
+            if (valuesStatus != null) {
+                for (List row : valuesStatus) {
+                    iteration += 1;
+                    if(iteration == rowCount){
+                        row.set(0, "Present");
+                    }
+                }
+            }
+
+            ValueRange body = new ValueRange()
+                    .setValues(valuesStatus);
+            UpdateValuesResponse result =
+                    mService.spreadsheets().values().update(spreadsheetId, rangeStatus, body)
+                            .setValueInputOption("RAW")
+                            .execute();
+            System.out.printf("%d cells updated.", result.getUpdatedCells());
+
+            return resultsStatus;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            verifyResult.setText("");
+        }
+
+        @Override
+        protected void onPostExecute(List<String> output) {
+            if (output == null || output.size() == 0) {
+                verifyResult.setText("No results returned.");
+            } else {
+
+            }
+        }
+
+        @Override
+        protected void onCancelled() {
+            if (mLastError != null) {
+                if (mLastError instanceof GooglePlayServicesAvailabilityIOException) {
+                    showGooglePlayServicesAvailabilityErrorDialog(
+                            ((GooglePlayServicesAvailabilityIOException) mLastError)
+                                    .getConnectionStatusCode());
+                } else if (mLastError instanceof UserRecoverableAuthIOException) {
+                    startActivityForResult(
+                            ((UserRecoverableAuthIOException) mLastError).getIntent(),
+                            MainActivity.REQUEST_AUTHORIZATION);
+                } else {
+
+                }
+            } else {
+                verifyResult.setText("Request cancelled.");
+            }
+        }
+
     }
 }
